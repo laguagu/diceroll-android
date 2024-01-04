a2e4f63198b07d5983decaecdba5d33a0de7d4ab
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-04 12:41:14 +0200

    Onclick listener and button functionality

--- app/src/main/java/com/example/diceroll/CalculatorActivity.kt
+++ app/src/main/java/com/example/diceroll/CalculatorActivity.kt
@@ -2,16 +2,23 @@ package com.example.diceroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.diceroll.databinding.ActivityCalculatorBinding
 
-class CalculatorActivity : AppCompatActivity() {
+class CalculatorActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityCalculatorBinding
     lateinit var btnAdd : Button
     lateinit var btnDec : Button
     lateinit var btnMultiply : Button
     lateinit var btnDivide : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var resultTv : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorBinding.inflate(layoutInflater)
@@ -20,5 +27,34 @@ class CalculatorActivity : AppCompatActivity() {
         btnDec = findViewById(R.id.btn_dec)
         btnMultiply = findViewById(R.id.btn_multiply)
         btnDivide = findViewById(R.id.btn_divide)
+        etA = findViewById(R.id.first_val)
+        etB = findViewById(R.id.second_val)
+        resultTv = findViewById(R.id.result_tv)
+
+        btnAdd.setOnClickListener(this)
+        btnDec.setOnClickListener(this)
+        btnMultiply.setOnClickListener(this)
+        btnDivide.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        var a = etA.text.toString().toDouble()
+        var b = etB.text.toString().toDouble()
+        var result = 0.0
+        when(v?.id){
+            R.id.btn_add -> {
+                result = a+b
+            }
+            R.id.btn_dec -> {
+                result = a-b
+            }
+            R.id.btn_multiply -> {
+                result = a*b
+            }
+            R.id.btn_divide -> {
+                result = a/b
+            }
+        }
+        resultTv.text = "Result is $result"
     }
 }
